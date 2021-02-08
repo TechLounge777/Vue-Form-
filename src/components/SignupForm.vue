759077f6880414f52eb986e5b2b99af7e93d7fdd
TechLounge777 <msblanche777@gmail.com>
2021-02-08 22:55:01 +0100

    added addSkill

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,16 +12,21 @@
           <option value="developer">Web Developer</option>
           <option value="developer">Web Designer</option>
         </select>
+
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">{{ skill }}
+        </div>
+
+        <div class="terms">
+          <input type="checkbox" required>
+          <label>Accept terms and conditions</label>
+        </div>
     </form>
     <p>Email: {{email}}</p>
     <p>Password: {{password}}</p>
     <p>Role: {{role}}</p>
-
-    <div class="terms">
-      <input type="checkbox" required>
-      <label>Accept terms and conditions</label>
-    </div>
-
+    <p>Terms accepted: {{terms}}</p>
 </template>
 
 <script>
@@ -31,7 +36,20 @@ export default {
       email: '',
       password: '',
       role: '',
+      terms: false,
+      tempSkill: '',
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ',' && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = '';
+      }
+    },
   },
 };
 </script>
